908ed60b504b106641547e9658ed090f0676cd9b
Viva Laas <vivalaas@udel.edu>
2020-11-24 12:59:06 -0500

    setting up the basic bugging

--- crowUI/src/app/app-routing.module.ts
+++ crowUI/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'welcome', component: WelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
