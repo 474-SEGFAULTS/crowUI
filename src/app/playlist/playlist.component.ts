c5054743217adcbe462f15c3affe3e7de514e15f
luckyruby1 <lisapat@udel.edu>
2020-11-25 12:39:08 -0500

    Starting template for playlist html & css

---
+++ src/app/playlist/playlist.component.ts
@@ -0,0 +1,16 @@
+import { Input } from '@angular/core';
+import { Component, OnInit, InputDecorator } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist',
+  templateUrl: './playlist.component.html',
+  styleUrls: ['./playlist.component.css']
+})
+export class PlaylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
