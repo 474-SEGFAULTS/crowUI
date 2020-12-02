cf65e28598d41d7510a3720383166bd291639713
luckyruby1 <lisapathania@yahoo.com>
2020-12-01 19:15:22 -0500

    Created artist page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArtistComponent } from './artist/artist.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 
 const routes: Routes = [ 
   {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'playlist', component: PlaylistComponent}
+  {path:'playlist', component: PlaylistComponent},
+  {path:'artist', component: ArtistComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { ArtistComponent } from './artist/artist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlaylistComponent
+    PlaylistComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
