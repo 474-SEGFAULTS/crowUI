5709ac955030315f60f3526507a9ed1429b6d78b
jvillemare <jvill@udel.edu>
2020-12-04 19:55:37 +0100

    merged crowUI/src into src

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ArtistComponent } from './artist/artist.component';
-import { PlaylistComponent } from './playlist/playlist.component';
 
-const routes: Routes = [ 
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'playlist', component: PlaylistComponent},
-  {path:'artist', component: ArtistComponent}
+import { DiscussionComponent } from './discussion/discussion.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { GenreComponent } from './genre/genre.component';
+import { AlbumComponent } from './album/album.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'welcome',pathMatch:'full'},
+  {path: '', component:DiscussionComponent}
+  {path:'welcome', component: WelcomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'login', component: LoginComponent},
+  {path:'signUp', component: SignUpComponent},
+  {path:'home', component: HomeComponent},
+  {path:'genres', component: GenreComponent},
+  {path:'album', component: AlbumComponent}
 ];
 
 @NgModule({
