1cf1c81fefc08a3bc22e6c0efb816957ed9d1966
Viva Laas <vivalaas@udel.edu>
2020-12-06 16:13:50 -0500

    bye bye top-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { HomeComponent } from './home/home.component';
 import { GenreComponent } from './genre/genre.component';
 import { AlbumComponent } from './album/album.component';
 import { MusicPlayerComponent } from './music-player/music-player.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { MusicPlayerBarComponent } from './music-player-bar/music-player-bar.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptorService } from './token-interceptor.service';
